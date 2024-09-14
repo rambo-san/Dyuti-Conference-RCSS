dbb5f95421fcd399391eec9fc8340ff8179f4613
rambo-san <abhisuresh2020@gmail.com>
2024-09-14 10:47:25 +0530

    x added at footer

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -145,6 +145,9 @@
                      <li>   <a href="https://www.facebook.com/p/Rajagiri-College-of-Social-Sciences-Autonomous-100067140653830/" target="_blank"> <i class="bi bi-facebook"></i> </a> </li>
                         <li>  <a href="https://www.instagram.com/rcss.live/?hl=en" target="_blank">  <i class="bi bi-instagram"></i>  </a> </li>
                         <li> <a href="https://in.linkedin.com/school/rajagiri-college-of-social-sciences-autonomous/?original_referer=https%3A%2F%2Fwww.google.com%2F" target="_blank"> <i class="bi bi-linkedin"></i>  </a> </li>
+                        <li>  <a href="https://x.com/DyutiRajagiri?s=09" target="_blank">  <i class="bi bi-twitter-x"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
+                           <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
+                         </svg></i> </a> </li>
                      </ul>
                   </div>
                </div>
